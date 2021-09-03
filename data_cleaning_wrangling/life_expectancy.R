6abbd994cd699a158fa93463ecc7c3257c62d837
prathibapradeep <prathiba.ratnasabesan@gmail.com>
2021-09-03 23:04:24 +0100

    Life Expectancy
    Intial Commit

---
+++ data_cleaning_wrangling/life_expectancy.R
@@ -0,0 +1,53 @@
+#--------------------------------------------------------------------------
+# This script is to load and clean the life expectancy data.
+#--------------------------------------------------------------------------
+
+# Load the library --------------------------------------------------------
+library(here)
+library(tidyverse)
+library(janitor)
+
+source(here("data_cleaning_wrangling/datazonelookup.R"))
+
+# Read the data file ------------------------------------------------------
+
+life_expectancy_clean <- read_csv(here("original_data/life_expectancy_raw_data.csv")) %>% clean_names()
+
+# Data Cleaning -----------------------------------------------------------
+
+# Tidy the data and rename the colummn names
+life_expectancy_clean <- life_expectancy_clean %>%
+  pivot_wider(
+    names_from = measurement,
+    values_from = value
+  ) %>%
+  rename(
+    le_lower_ci = "95% Lower Confidence Limit",
+    le_upper_ci = "95% Upper Confidence Limit",
+    le_value = "Count"
+  )
+# Update the age column
+life_expectancy_clean <- life_expectancy_clean %>%
+  mutate(age = str_replace(age, " years", ""))
+
+# Lookup the Area name using data_zone_lookup_code
+life_expectancy_clean <- life_expectancy_clean %>%
+  left_join(data_zone_lookup_code_names, by = c("feature_code" = "code")) %>%
+  select(feature_code, name, type, date_code, sex, age, simd_quintiles,
+         urban_rural_classification, le_lower_ci, le_upper_ci, le_value)
+
+# Rename the type
+life_expectancy_clean <- life_expectancy_clean %>%
+  mutate(type = recode(type,
+    "la" = "Local Authority",
+    "hb" = "NHS Health Board",
+    "country" = "Scotland",
+    "spc" = "Scottish Parliamentary Constituencies"
+  ))
+
+# Write the clean data ----------------------------------------------------
+
+# Write the cleaned data into clean_data/folder
+write_csv(life_expectancy_clean, here("clean_data/life_expectancy.csv"))
+
+# End of code -------------------------------------------------------------

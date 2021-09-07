24e161bd89eb43cc3de11f0e2917c48e29f0bb32
paddyhudson <87127584+paddyhudson@users.noreply.github.com>
2021-09-07 23:26:02 +0100

    la_zones

--- shiny_app/mapping_experimental.R
+++ shiny_app/mapping_experimental.R
@@ -9,27 +9,47 @@
 library(leaflet)
 library(sf)
 library(tidyverse)
+library(here)
 
 #load data - update filepath as necessary
 
 hb_zones <- read_sf(here("clean_data/hb_zones/hb_zones_simple.shp"))
 le <- read_csv("clean_data/life_expectancy.csv")
 
+#Get Scottish Average
+
+scot_ave <- le %>% 
+  filter(
+    date_code == "2017-2019" &
+      age == 0 &
+      type == "Scotland" &
+      sex == "All"
+  ) %>% 
+  select(le_value) %>% 
+  pull()
+
 #filter le data
 
 le_filtered <- le %>% 
   filter(
     date_code == "2017-2019" &
-      age == 0 &
-      type == "NHS Health Board" &
-      sex == "All"
+    age == 0 &
+    type == "NHS Health Board" &
+    sex == "All"
   )
   
 #Join spatial data to health data
 
 map_data <- hb_zones %>% 
   left_join(le_filtered, by = c("hb_code" = "feature_code")) %>% 
-  select(-name)
+  select(-name) %>% 
+  mutate(label = "LE")
+
+#Create comparative data
+
+map_data <- map_data %>%
+  mutate(le_value = le_value - scot_ave) %>%
+  mutate(label = "LE - Scottish Average")
 
 #Create colour palette
 
@@ -37,10 +57,11 @@ map_palette <- colorNumeric("plasma", domain = range(map_data$le_value))
   
 #Create map object with polygons and name popups
   
-hb_map <- map_data %>% 
+#hb_map <- 
+  map_data %>% 
   leaflet() %>% 
   #addTiles() %>% #uncomment this line to add background map
   addPolygons(
-    popup = ~str_c(hb_name, le_value, sep = " "),
+    popup = ~str_c(hb_name, "<br>", label, " = ", round(le_value, 2), sep = ""),
     color = ~map_palette(le_value)
   )

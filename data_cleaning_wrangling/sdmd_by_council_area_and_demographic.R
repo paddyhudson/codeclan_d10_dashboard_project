f28175678d2783cb9a6c8dfe6803d94052ea0828
Jonathan Parker <malinkin@msn.com>
2021-09-07 14:45:36 +0100

    Changes to data cleaning/wrangling script for drug data; new cleaned data set for drug data; moved legacy clean drugs dataset to archive

--- data_cleaning_wrangling/sdmd_by_council_area_and_demographic.R
+++ data_cleaning_wrangling/sdmd_by_council_area_and_demographic.R
@@ -5,31 +5,55 @@
 # cleaned by Derek H
 
 # Information on the number of individuals presenting for assessment at specialist drug 
-# treatment services in Scotland presented at council area level, broken down by age and sex.
+# treatment services in Scotland presented at council area and health board levels, 
+# broken down by age and sex.
 
 # Load the library --------------------------------------------------------
 library(here)
 library(tidyverse)
 library(janitor)
 
-# Read the data file ------------------------------------------------------
+# Read in the data files and clean names ---------------------------------------
 
 source(here("data_cleaning_wrangling/datazonelookup.R"))
 
-sdmd_by_ca_and_demo_clean <- read_csv(here("original_data/sdmd_by_council_area_and_demographics.csv")) %>% 
+# local authority-level data
+sdmd_by_ca_and_demo_clean <- 
+  read_csv(here("original_data/sdmd_by_council_area_and_demographics.csv")) %>% 
+  clean_names()
+
+# health board-level data
+sdmd_by_hb_and_demo_clean <- 
+  read_csv(here("original_data/sdmd_by_health_board_and_demographics.csv")) %>% 
   clean_names()
 
 # Data Cleaning -----------------------------------------------------------
 
-# joined sdmd dataset with data_zone_lookup_code dataset
-# select relevant variables
-# Rename variables to make them more descriptive
-# Recode type column
-
+# rename the 'ca' (from the local authority-level data) and 'hbr' (from the health
+# board-level data) variables to 'feature_code' to enable joining.
+# remove qualifier variables ('caqf' and 'hbrqf') as not required for joining or analysis.
 sdmd_by_ca_and_demo_clean <- sdmd_by_ca_and_demo_clean %>% 
-  left_join(data_zone_lookup_code_names, by = c("ca" = "code")) %>% 
-  select(ca, name, type, financial_year, age_group, sex, number_assessed) %>% 
-  rename(feature_code = ca, year = financial_year) %>% 
+  rename(feature_code = ca) %>% 
+  select(-caqf)
+
+sdmd_by_hb_and_demo_clean <- sdmd_by_hb_and_demo_clean %>% 
+  rename(feature_code = hbr) %>% 
+  select(-hbrqf)
+
+# join both sdmd datasets to create one showing data for both local authorities
+# and health boards
+sdmd_combined_clean <-
+bind_rows(sdmd_by_ca_and_demo_clean, sdmd_by_hb_and_demo_clean)
+
+
+# join combined sdmd dataset with 'data_zone_lookup_code' dataset
+sdmd_combined_plus_zones <- sdmd_combined_clean %>% 
+  left_join(data_zone_lookup_code_names, by = c("feature_code" = "code")) %>% 
+  # rename variables to match data sets for other priority areas
+  rename(year = financial_year, age = age_group) %>% 
+  # select relevant variables
+  select(feature_code, name, type, year, age, sex, number_assessed) %>% 
+  # recode type column
   mutate(type = recode(type,
                        "la" = "Local Authority",
                        "hb" = "NHS Health Board",
@@ -38,23 +62,24 @@ sdmd_by_ca_and_demo_clean <- sdmd_by_ca_and_demo_clean %>%
   ))
 
 # commented out code to convert 20XX/XX to YYYY
-# year = as.integer(recode(year, "2006/07" = "2006",
-#                          "2007/08" = "2007",
-#                          "2008/09" = "2008",
-#                          "2009/10" = "2009",
-#                          "2010/11" = "2010",
-#                          "2011/12" = "2011",
-#                          "2012/13" = "2012",
-#                          "2013/14" = "2013",
-#                          "2014/15" = "2014",
-#                          "2015/16" = "2015",
-#                          "2016/17" = "2016",
-#                          "2017/18" = "2017",
+# year = as.integer(recode(year, "2006/07" = "2007",
+#                          "2007/08" = "2008",
+#                          "2008/09" = "2009",
+#                          "2009/10" = "2010",
+#                          "2010/11" = "2011",
+#                          "2011/12" = "2012",
+#                          "2012/13" = "2013",
+#                          "2013/14" = "2014",
+#                          "2014/15" = "2015",
+#                          "2015/16" = "2016",
+#                          "2016/17" = "2017",
+#                          "2017/18" = "2018",
 #                          "2018/19" = "2019"))
 
 # Write the clean data ----------------------------------------------------
 
 # Write the cleaned data into clean_data/folder
-write_csv(sdmd_by_ca_and_demo_clean, here("clean_data/sdmd_by_ca_and_demo_clean.csv"))
+
+write_csv(sdmd_combined_plus_zones, "clean_data/sdmd_combined_plus_zones.csv")
 
 # End of code -------------------------------------------------------------

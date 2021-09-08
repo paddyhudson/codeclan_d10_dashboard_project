1fd3bc49b603ba353b2240830adf4da3c6924c95
paddyhudson <87127584+paddyhudson@users.noreply.github.com>
2021-09-08 15:14:53 +0100

    ui_map_working!!!

--- shiny_app/server.R
+++ shiny_app/server.R
@@ -216,67 +216,24 @@ observeEvent(input$topic_input, {
 # Server script for Smoking  --------------------------------------------
 
 # Server script for Map  ------------------------------------------------
-          
-          #choose spatial data
-          # spatial_data <- switch(input$area_input,
-          #                        "NHS Health Board" = hb_zones,
-          #                        "Local Authority" = la_zones
-          # )
-          # 
-          # #choose topic data
-          # topic_data <- switch(input$topic_input,
-          #                      "Life Expectancy" = life_expectancy_clean,
-          #                      "Drug Abuse" = sdmd_combined_plus_zones,
-          #                      "Smoking" = smoking_clean
-          # ) %>%
-          #   filter(type == input$area_input,
-          #          sex == "All",
-          #          age == switch(input$topic_input,
-          #                        "Life Expectancy" = 0,
-          #                        "Drug Abuse" = "All",
-          #                        "Smoking" = "All"
-          #          )
-          #   ) %>% 
-          #   select(-name)
-          # 
-          # if (input$topic_input == "Life Expectancy"){
-          #   topic_data <- topic_data %>% 
-          #     filter(date_code == "2017-2019") %>% 
-          #     rename(value = le_value) %>% 
-          #     mutate(label = "Life Expectancy")
-          # } else if (input$topic_input == "Drug Abuse"){
-          #   topic_data <- topic_data %>% 
-          #     filter(year == "2017/18") %>% 
-          #     rename(value = number_assessed) %>% 
-          #     mutate(label = "Number Assessed")
-          # } else if (input$topic_input == "Smoking"){
-          #    topic_data <- topic_data %>%
-          #     filter(date_code == 2019,
-          #        long_term_condition == "All",
-          #        household_type == "All",
-          #        smokes == "Yes"
-          #     ) %>% 
-          #     rename(value = sm_percent) %>% 
-          #     mutate(label = "Percentage of Population who are Smokers")
-          # }
-          # 
-          # #join to get map data
-          # map_data <- spatial_data %>% 
-          #   left_join(topic_data, by = c("code" = "feature_code"))
-          # 
-          # #Create colour palette
-          # map_palette <- colorNumeric("magma", domain = range(map_data$value))
-          # 
-          # #get the map
-          # output$my_map <- renderLeaflet({
-          # map_data %>% 
-          #   leaflet() %>% 
-          #   #addTiles() %>% #uncomment this line to add background map
-          #   addPolygons(
-          #     popup = ~str_c(name, "<br>", label, " = ", round(value, 2), sep = ""),
-          #     color = ~map_palette(value)
-          #   )
-          # })
+map_area_input <- reactive(input$map_area_input)
+map_topic_input <- reactive(input$map_topic_input)
+map_data <- reactive(get_map_data(map_topic_input(), map_area_input()))
+
+          #Create colour palette
+          map_palette <- reactive(
+            colorNumeric("magma", domain = range(map_data()$value)))
+
+          #get the map
+          output$my_map <- renderLeaflet({
+            map_data() %>%
+              leaflet() %>%
+              #addTiles() %>% #uncomment this line to add background map
+              addPolygons(
+                popup = ~ str_c(name, "<br>", label, " = ", round(value, 2), sep = ""),
+               # color = ~ map_palette(value)
+              )
+          })
           
 # Download script   -----------------------------------------------------        
 output$download_report <- downloadHandler(
--- shiny_app/ui_map.R
+++ shiny_app/ui_map.R
@@ -5,70 +5,63 @@
 # #--------------------------------------------------------------------------#
 # # 1.0             | Paddy       | Initial Version                          #
 # #--------------------------------------------------------------------------#
-# 
-# library (shinyWidgets)
-# 
-# ui <- fluidPage(
-#   #style = "background-color: #a6cbe3" ,
-#   #titlePanel(tags$h2("Map")),
-#   
-#   sidebarLayout(
-#     sidebarPanel(#style = "background-color: #a6cbe3" ,
-#       #Input the Topic
-#       fluidRow(
-#         selectInput("map_topic_input",
-#                     "Select topic",
-#                     choices = c("Life Expectancy", "Drug Abuse","Smoking")
-#                     
-#         )),
-#       fluidRow(
-#         selectInput("map_area_input",
-#                     "Select Data Zone",
-#                     choices = c("NHS Health Board", "Local Authority")
-#                     
-#         )),
-#       
-#       fluidRow(
-#         selectInput("map_name_input",
-#                     "Select Region:",
-#                     choices = NULL
-#         ),
-#       ),
-#       fluidRow(
-#         selectInput("map_breakdown_input",
-#                     "Select breakdown:",
-#                     choices = NULL
-#         )
-#       ),
-#       fluidRow(tags$br()),
-#       fluidRow(tags$br())
-#     ),
-#     mainPanel(
-#       #Content to display the map
-#       fluidRow(
-#         box(   
-#           title = tags$h3(textOutput("topic", inline = TRUE),
-#                           " based on ",
-#                           textOutput("name", inline = TRUE),
-#                           " for the ",
-#                           textOutput("breakdown", inline = TRUE),
-#                           " Category  "
-#           ),
-#           width = 12, status = "primary",
-#           leafletOutput("my_map")
-#         )
-#         
-#       ),
-#       
-#       #Content to display the table
-#       fluidRow(
-#         box(
-#           width = 12,
-#           title = tags$h3("Table"),
-#           dataTableOutput("map_output_table")
-#         )
-#       )
-#       
-#     )
-#   )
-# )
+
+library (shinyWidgets)
+
+ui <- fluidPage(
+  #style = "background-color: #a6cbe3" ,
+  titlePanel(tags$h2("Map")),
+
+  sidebarLayout(
+    sidebarPanel(#style = "background-color: #a6cbe3" ,
+      #Input the Topic
+      fluidRow(
+        selectInput("map_topic_input",
+                    "Select topic",
+                    choices = c("Life Expectancy", "Drug Abuse","Smoking")
+
+        )),
+      fluidRow(
+        selectInput("map_area_input",
+                    "Select Data Zone",
+                    choices = c("NHS Health Board", "Local Authority")
+
+        )),
+      fluidRow(
+        selectInput("map_breakdown_input",
+                    "Select breakdown:",
+                    choices = c("All", "Age", "Gender")
+        )
+      ),
+      fluidRow(tags$br()),
+      fluidRow(tags$br())
+    ),
+    mainPanel(
+      #Content to display the map
+      fluidRow(
+        box(
+          title = tags$h3(textOutput("map_input_topic", inline = TRUE),
+                          " based on ",
+                          textOutput("map_input_name", inline = TRUE),
+                          " for the ",
+                          textOutput("map_input_breakdown", inline = TRUE),
+                          " Category  "
+          ),
+          width = 12, status = "primary",
+          leafletOutput("my_map")
+        )
+
+      ),
+
+      #Content to display the table
+      fluidRow(
+        box(
+          width = 12,
+          title = tags$h3("Table"),
+          dataTableOutput("map_output_table")
+        )
+      )
+
+    )
+  )
+)
